7817b46084056d15dc6ee2aeba1c019d2608ae72
Joelle <francois.joelle1@gmail.com>
2019-04-12 11:53:42 +0200

    Création base de données particulier et pro

---
+++ src/Entity/Particulier.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ParticulierRepository")
+ */
+class Particulier
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Prenom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Adresse;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $CP;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Email;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $MotDePasse;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->Nom;
+    }
+
+    public function setNom(string $Nom): self
+    {
+        $this->Nom = $Nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->Prenom;
+    }
+
+    public function setPrenom(string $Prenom): self
+    {
+        $this->Prenom = $Prenom;
+
+        return $this;
+    }
+
+    public function getAdresse(): ?string
+    {
+        return $this->Adresse;
+    }
+
+    public function setAdresse(string $Adresse): self
+    {
+        $this->Adresse = $Adresse;
+
+        return $this;
+    }
+
+    public function getCP(): ?int
+    {
+        return $this->CP;
+    }
+
+    public function setCP(int $CP): self
+    {
+        $this->CP = $CP;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->Email;
+    }
+
+    public function setEmail(string $Email): self
+    {
+        $this->Email = $Email;
+
+        return $this;
+    }
+
+    public function getMotDePasse(): ?string
+    {
+        return $this->MotDePasse;
+    }
+
+    public function setMotDePasse(string $MotDePasse): self
+    {
+        $this->MotDePasse = $MotDePasse;
+
+        return $this;
+    }
+}
