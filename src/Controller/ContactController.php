b0784fa92cb866598b8f8219778b3b36dd7a3032
Teddy <teddy@tdipointcom.fr>
2019-04-15 10:48:43 +0200

    Merge remote-tracking branch 'origin/master'

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ContactParticulier;
+use App\Entity\ContactPro;
+use App\Form\ContactParticulierType;
+use App\Form\ContactProType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact/particulier", name="contact_particulier")
+     * @param Request $request
+     * @return Response
+     */
+    public function ContactParticulier(Request $request)
+    {
+        $contactparticulier = new ContactParticulier();
+        $form = $this->createForm(ContactParticulierType::class, $contactparticulier);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactparticulier->setDate(new \DateTime('now'));
+            // Ajouter du message en BDD
+            $manager = $this->getDoctrine()->getManager();
+
+            $manager->persist($contactparticulier);
+            $manager->flush();
+        }
+
+        return $this->render('contact/contactparticulier.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/contact/pro", name="contact_pro")
+     * @param Request $request
+     * @return Response
+     */
+    public function ContactPro(Request $request)
+    {
+        $contactpro = new ContactPro();
+        $form = $this->createForm(ContactProType::class, $contactpro);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contactpro->setDate(new \DateTime('now'));
+            // Ajouter du message en BDD
+            $manager = $this->getDoctrine()->getManager();
+
+            $manager->persist($contactpro);
+            $manager->flush();
+        }
+
+        return $this->render('contact/contactpro.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Controller/RegistrationProController.php
@@ -0,0 +1,39 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Pro;
+use App\Form\RegisterProType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class RegistrationProController extends AbstractController
+{
+    /**
+     * @Route("/inscription/professionel", name="inscription_professionel")
+     * @param Request $request
+     * @return Response
+     */
+    public function ProRegistration(Request $request)
+    {
+        $professionel = new Pro();
+        $form = $this->createForm(RegisterProType::class, $professionel);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // Ajouter l'utilisateur en BDD
+            $manager = $this->getDoctrine()->getManager();
+
+            $manager->persist($professionel);
+            $manager->flush();
+        }
+
+        return $this->render('registration/ProRegistration.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+}
