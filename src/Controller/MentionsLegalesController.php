06be57ff4fcf8bd424db0453cbecc51a0dc7e080
Joelle <francois.joelle1@gmail.com>
2019-04-15 16:53:02 +0200

    Ajout page "qui sommes nous ?"
    + modif nom des controllers mentionslegales et cgu

--- src/Controller/MentionsLegalesController.php
+++ src/Controller/MentionsLegalesController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MentionsLegales extends AbstractController
+class MentionsLegalesController extends AbstractController
 {
     /**
      * @Route("/MentionsLegales", name="mentions_legales")
