78249a8f40f564eca4b39b8877f5089370e6d080
Teddy <teddy@tdipointcom.fr>
2019-04-15 14:01:47 +0200

    Mise à jour
    Footer/Particulier & Professionnel

--- src/Controller/ProfessionnelController.php
+++ src/Controller/ProfessionnelController.php
@@ -5,12 +5,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 
-class ProfessionnelController
+class ProfessionnelController extends AbstractController
 {
     /**
      * @Route("/", name="professionnel")
+     * @return Response
      */
     public function index()
     {
