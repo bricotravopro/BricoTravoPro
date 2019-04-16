ec7e55706e08c63e932c2afbf2510a8cd5520c1f
teddy <teddy@tdipointcom.fr>
2019-04-16 15:15:56 +0200

    1st upload du 16avr
    page blog mise en page + lien dans footer

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -5,8 +5,17 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
 
 class BlogController extends AbstractController
 {
-
+    /**
+     * @Route("/blog", name="blog")
+     * @return Response
+     */
+    public function index()
+    {
+        return $this->render('blog/blog.html.twig');
+    }
 }
