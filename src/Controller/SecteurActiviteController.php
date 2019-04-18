a5cce29190d44c2ec532634ff8bf202bef8ceb66
Joelle <francois.joelle1@gmail.com>
2019-04-18 10:39:37 +0200

    ajout passwordtype

--- src/Controller/SecteurActiviteController.php
+++ src/Controller/SecteurActiviteController.php
@@ -4,7 +4,7 @@
 namespace App\Controller;
 
 
-use App\Repository\SecteurActiviteRepository;
+use App\Entity\SecteurActivite;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -15,11 +15,18 @@ class SecteurActiviteController extends AbstractController
      * @Route("/secteuractivité", name="SecteurActivite")
      * @return Response
      */
-    public function SecteurActivite(SecteurActiviteRepository $repository)
-    {
-        $SecteurActivite = $repository->findAll();
+    public function showAllActivite(){
+        $entityManager = $this->getDoctrine()->getManager();
+        $activite = $entityManager->getRepository(SecteurActivite::class)->findAll();
 
-        return $this->render('Search/SecteurActivite.html.twig');
+// On exécute la requête
+        $entityManager->flush();
+
+        return $this->render('Search/SecteurActivite.html.twig',[
+
+        ]);
     }
+
+
 
 }
