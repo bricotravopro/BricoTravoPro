2b6badcf7b464f86d0def3b880019aa97fb40ad1
teddy <teddy@tdipointcom.fr>
2019-04-19 16:14:48 +0200

    Blog
    Alimentation / Mise en page

---
+++ src/Entity/EmailNewsletter.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Entity
+ * @UniqueEntity(fields="email", message="Email déjà enregistré")
+ */
+class EmailNewsletter
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, unique=true)
+     * @Assert\NotBlank()
+     * @Assert\Email()
+     */
+    private $email;
+
+    // méthode
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function eraseCredentials()
+    {
+    }
+}
+
