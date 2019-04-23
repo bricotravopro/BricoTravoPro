86c778168eca39611c108afc0aee0e8cd6032f04
teddy <teddy@tdipointcom.fr>
2019-04-23 11:05:32 +0200

    Taf en cours
    Carousel

--- public/js/carousel.js
+++ public/js/carousel.js
@@ -2,3 +2,9 @@
 $('#blogCarousel').carousel({
     interval: 2000
 });
+$('#playButton').click(function () {
+    $('#myCarousel').carousel('cycle');
+});
+$('#pauseButton').click(function () {
+    $('#myCarousel').carousel('pause');
+});
