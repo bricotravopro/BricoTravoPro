b932c0f033eaaf841232ca568a22884068aec1c9
Marchoix Thomas <marchoix.thomas@gmail.com>
2019-04-24 16:12:48 +0200

    Liste déroulante dans la recherche

--- src/Form/RechercheArtisanType.php
+++ src/Form/RechercheArtisanType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 
 class RechercheArtisanType extends AbstractType
@@ -12,7 +13,18 @@ class RechercheArtisanType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('SecteurActivite')
+            ->add('SecteurActivite', ChoiceType::class, [
+                'choices' => [
+                    'Plomberie' => 'Plomberie',
+                    'Charpente' => 'Charpente',
+                    'Couvreur/Toiture' => 'Couvreur/Toiture',
+                    'Electricité' => 'Electricité',
+                    'Espaces Verts' => 'Espaces Verts',
+                    'Maçonnerie' => 'Maçonnerie',
+                    'Peinture' => 'Peinture',
+                    'Revêtements et sols' => 'Revêtements et sols',
+                    'Menuiserie' => 'Menuiserie',
+                ]])
             ->add('Ville');
     }
 }
