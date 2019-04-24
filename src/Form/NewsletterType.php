5d05274b193ef2393f26e95ca365aaab2fb600c7
teddy <teddy@tdipointcom.fr>
2019-04-24 10:43:43 +0200

    Module Newsletter

--- src/Form/NewsletterType.php
+++ src/Form/NewsletterType.php
@@ -1,27 +1,28 @@
 <?php
 
+
 namespace App\Form;
 
-use App\Entity\ContactParticulier;
-use Symfony\Component\Form\AbstractType;
+
+use App\Entity\EmailNewsletter;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ContactParticulierType extends AbstractType
+class NewsletterType extends AbstractController
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-
         $builder
-            ->add('Sujet')
-            ->add('Message')
+            ->add('email', EmailType::class)
+            ->getForm()
         ;
     }
-
     public function configureOptions(OptionsResolver $resolver)
     {
         $resolver->setDefaults([
-            'data_class' => ContactParticulier::class,
+            'data_class' => EmailNewsletter::class
         ]);
     }
 }
