83a0f10f3f5ddaaf80dc8340ffa6dd8720a42dc6
teddy <teddy@tdipointcom.fr>
2019-04-24 10:43:43 +0200

    Module Newsletter

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+
+use App\Entity\EmailNewsletter;
+use App\Form\NewsletterType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -11,9 +15,24 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index()
+    public function index(Request $request)
     {
-        return $this->render('home/index.html.twig');
+        $emailnewsletter= new EmailNewsletter();
+        $form = $this->createForm(NewsletterType::class, $emailnewsletter);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Ajouter du message en BDD
+            $manager = $this->getDoctrine()->getManager();
+
+            $manager->persist($emailnewsletter);
+            $manager->flush();
+        }
+        return $this->render('home/index.html.twig',
+            ['form' =>$form->createView()]);
     }
+
+
 
 }
