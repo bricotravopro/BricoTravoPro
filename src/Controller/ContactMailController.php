5d9695c7625f391f872976e632764a8edacc57ba
teddy <teddy@tdipointcom.fr>
2019-04-24 13:26:17 +0200

    Contact Mail
    Réparation

--- src/Controller/ContactMailController.php
+++ src/Controller/ContactMailController.php
@@ -9,12 +9,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 
-class ContactMailController
+class ContactMailController extends AbstractController
 {
     /**
      * @Route("/contact-mail", name="contact-mail")
+     * @param Request $request
      * @return Response
      */
     public function ContactMail(Request $request)
