75a2040f4e1ebe86416d94a8fa4266e983b07644
teddy <teddy@tdipointcom.fr>
2019-04-25 10:45:49 +0200

    MAJ formulaires
    Placeholdrer + Label

--- src/Form/ContactMailType.php
+++ src/Form/ContactMailType.php
@@ -5,7 +5,10 @@ namespace App\Form;
 use App\Entity\ContactMail;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,9 +17,9 @@ class ContactMailType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('Nom')
-            ->add('Prenom')
-            ->add('Email')
+            ->add('Nom', TextType::class, array('label'=>false, 'attr'=>array('placeholder'=>'Saisissez ici votre Nom (requis)')))
+            ->add('Prenom', TextType::class, array('label'=>false, 'attr'=>array('placeholder'=>'Saisissez ici votre Prénom')))
+            ->add('Email', EmailType::class, array('label'=>false, 'attr'=>array('placeholder'=>'Saisissez ici votre e-mail (requis)')))
             ->add('Sujet', ChoiceType::class, [
                 'choices'  => [
                     'Choisissez votre sujet' => 'Choisissez votre sujet',
@@ -25,7 +28,7 @@ class ContactMailType extends AbstractType
                     'Demande d\'entretien téléphonique' => 'Demande d\'entretien téléphonique',
                     'Autre' => 'Autre']
                 ])
-            ->add('Message', TextareaType::class)
+            ->add('Message', TextareaType::class, array('attr'=>array('placeholder'=>'La parole est à vous ...')))
         ;
     }
     public function configureOptions(OptionsResolver $resolver)
